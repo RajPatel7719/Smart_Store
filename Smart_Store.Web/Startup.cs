1668ea568e1238907b45bdc15f5bbb7fdf97cf3c
RajPatel <rvpatel7719@gmail.com>
2021-08-16 14:02:32 +0530

    Add project files.

---
+++ Smart_Store.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Smart_Store.Web.Startup))]
+namespace Smart_Store.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
